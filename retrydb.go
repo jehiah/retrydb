500d375c7998875ae87a3efc011aa6584c6ddb3f
Jehiah Czebotar <jehiah@gmail.com>
2015-01-21 22:49:47 -0500

    implement QueryRow()

--- retrydb.go
+++ retrydb.go
@@ -3,6 +3,7 @@ package retrydb
 import (
 	"database/sql"
 	"database/sql/driver"
+	"errors"
 	"log"
 )
 
@@ -60,9 +61,15 @@ func (db *RetryDB) Query(query string, args ...interface{}) (*sql.Rows, error) {
 	}
 	return rows, err
 }
-func (db *RetryDB) QueryRow(query string, args ...interface{}) *sql.Row {
-	panic("not implemented")
+
+// QueryRow executes a query that is expected to return at most one row.
+// QueryRow always return a non-nil value. Errors are deferred until
+// Row's Scan method is called.
+func (db *RetryDB) QueryRow(query string, args ...interface{}) *Row {
+	rows, err := db.Query(query, args...)
+	return &Row{rows: rows, err: err}
 }
+
 func (db *RetryDB) Close() error {
 	err := db.Primary.Close()
 	var sErr error
@@ -73,4 +80,58 @@ func (db *RetryDB) Close() error {
 		return err
 	}
 	return sErr
+}
+
+// Row is the result of calling QueryRow to select a single row.
+type Row struct {
+	// One of these two will be non-nil:
+	err  error // deferred error for easy chaining
+	rows *sql.Rows
+}
+
+// Scan copies the columns from the matched row into the values
+// pointed at by dest.  If more than one row matches the query,
+// Scan uses the first row and discards the rest.  If no row matches
+// the query, Scan returns ErrNoRows.
+func (r *Row) Scan(dest ...interface{}) error {
+	if r.err != nil {
+		return r.err
+	}
+
+	// TODO(bradfitz): for now we need to defensively clone all
+	// []byte that the driver returned (not permitting
+	// *RawBytes in Rows.Scan), since we're about to close
+	// the Rows in our defer, when we return from this function.
+	// the contract with the driver.Next(...) interface is that it
+	// can return slices into read-only temporary memory that's
+	// only valid until the next Scan/Close.  But the TODO is that
+	// for a lot of drivers, this copy will be unnecessary.  We
+	// should provide an optional interface for drivers to
+	// implement to say, "don't worry, the []bytes that I return
+	// from Next will not be modified again." (for instance, if
+	// they were obtained from the network anyway) But for now we
+	// don't care.
+	defer r.rows.Close()
+	for _, dp := range dest {
+		if _, ok := dp.(*sql.RawBytes); ok {
+			return errors.New("sql: RawBytes isn't allowed on Row.Scan")
+		}
+	}
+
+	if !r.rows.Next() {
+		if err := r.rows.Err(); err != nil {
+			return err
+		}
+		return sql.ErrNoRows
+	}
+	err := r.rows.Scan(dest...)
+	if err != nil {
+		return err
+	}
+	// Make sure the query can be processed to completion with no errors.
+	if err := r.rows.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
